7cd6161b22d3e50ec5a456e6f16599aee30e8d8d
SanyaSosedskyi <sanya1sosedskiy@gmail.com>
2019-12-29 06:01:56 +0200

    Refactor architecture and finish GUI

--- usermanagement/src/main/java/ua/nure/kn/susidskyi/usermanagement/db/ConnectionFactory.java
+++
@@ -1,8 +0,0 @@
-package ua.nure.kn.susidskyi.usermanagement.db;
-
-import java.sql.Connection;
-
-public interface ConnectionFactory {
-	Connection createConnection() throws DatabaseException;
-	
-}
